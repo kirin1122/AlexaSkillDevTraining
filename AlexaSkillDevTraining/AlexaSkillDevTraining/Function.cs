2f72c00cdb697e1a1fdbc10c3c09e44378c32fd8
y-terada@t-answer.jp <46492377+kirin1122@users.noreply.github.com>
2022-01-26 00:51:07 +0900

    update:サンプル打ち込み

--- AlexaSkillDevTraining/AlexaSkillDevTraining/Function.cs
+++ AlexaSkillDevTraining/AlexaSkillDevTraining/Function.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +14,66 @@ namespace AlexaSkillDevTraining
 {
     public class Function
     {
+
+        string[] messages = { 
+            "Hello World",
+            "Hello Alexa From CSharp",
+            "C#で実装したLambdaコードからこんにちは！"
+        };
+
         
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public  SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            string msg;
+
+            switch (input.Request)
+            {
+                case LaunchRequest Ir:
+                    msg = "こんにちは";
+                    break;
+
+                case IntentRequest ir:
+                    msg = MakeResponse(ir);
+                    break;
+                default:
+                    msg = "?";
+                    break;
+            }
+
+            var response = new SkillResponse 
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            response.Response.OutputSpeech = new PlainTextOutputSpeech() { Text = msg };
+
+            return response;
+        }
+
+        private string MakeResponse(IntentRequest ir)
+        {
+            string msg;
+
+            switch (ir.Intent.Name) 
+            {
+                case "Hello Alexa From CSharp":
+                    msg = messages[new Random().Next(3)];
+                    break;
+
+                default:
+                    msg = "?????????";
+                    break;
+            }
+
+            return msg;
         }
     }
 }
